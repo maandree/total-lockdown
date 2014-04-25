7a015aee68c5b7390fcff80f91621a881bdcbc73
Mattias Andrée <maandree@operamail.com>
2014-04-25 19:02:07 +0200

    work on identify what all keys are
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/program.c
+++ src/program.c
@@ -42,29 +42,30 @@ void verifier(int fd);
 const char* KVAL_MAP[15][256] = {
   [KT_LATIN]  = {NULL},
   [KT_LETTER] = {NULL},
-  [KT_FN]     = {NULL /* There are 256 keys here, see <linux/keyboard.h> if you need any of them. */},
-  [KT_SPEC]   = {[KVAL(K_HOLE)] = NULL /* No assignment */,
-		 [KVAL(K_ENTER)] = "\n",
-		 [KVAL(K_SH_REGS)] = NULL /* TODO: What is this? */,
-		 [KVAL(K_SH_MEM)] = NULL /* TODO: What is this? */,
-		 [KVAL(K_SH_STAT)] = NULL /* TODO: What is this? */,
-		 [KVAL(K_BREAK)] = NULL /* Break (pause), no output */,
-		 [KVAL(K_CONS)] = NULL /* TODO: What is this? */,
-		 [KVAL(K_CAPS)] = NULL /* TODO: What is this? */,
-		 [KVAL(K_NUM)] = NULL /* TODO: What is this? */,
-		 [KVAL(K_HOLD)] = NULL /* TODO: What is this? */,
-		 [KVAL(K_SCROLLFORW)] = NULL /* TODO: What is this? */,
-		 [KVAL(K_SCROLLBACK)] = NULL /* TODO: What is this? */,
-		 [KVAL(K_BOOT)] = NULL /* TODO: What is this? */,
-		 [KVAL(K_CAPSON)] = NULL /* TODO: What is this? */,
-		 [KVAL(K_COMPOSE)] = NULL /* Compose key (TODO) */,
-		 [KVAL(K_SAK)] = NULL /* TODO: What is this? */,
-		 [KVAL(K_DECRCONSOLE)] = NULL /* Switch to previous VT. Fat chance! */,
-		 [KVAL(K_INCRCONSOLE)] = NULL /* Switch to next VT. Fat chance! */,
+  [KT_FN]     = {NULL /* There are symbols keys here, see <linux/keyboard.h> if you need any of them. */},
+  [KT_SPEC]   = {[KVAL(K_HOLE)]         = NULL /* No assignment */,
+		 [KVAL(K_ENTER)]        = "\n",
+		 [KVAL(K_SH_REGS)]      = NULL /* Alternative graph + Scroll Lock, do nothing */,
+		 [KVAL(K_SH_MEM)]       = NULL /* Shift + Scroll Lock, do nothing */,
+		 [KVAL(K_SH_STAT)]      = NULL /* Control + Scroll Lock, do nothing */,
+		 [KVAL(K_BREAK)]        = NULL /* Break (pause), do nothing */,
+		 [KVAL(K_CONS)]         = NULL /* TODO: What is this? */,
+		 [KVAL(K_CAPS)]         = NULL /* Royal Canterlot Voice, ignore it */,
+		 [KVAL(K_NUM)]          = NULL,
+		 [KVAL(K_HOLD)]         = NULL /* Scroll Lock, do nothing */,
+		 [KVAL(K_SCROLLFORW)]   = NULL /* Shift-next, scroll down, do nothing  */,
+		 [KVAL(K_SCROLLBACK)]   = NULL /* Shift-prior, scroll up, do nothing */,
+		 [KVAL(K_BOOT)]         = NULL /* Three finger salute, ignore it */,
+		 [KVAL(K_CAPSON)]       = NULL /* TODO: What is this? */,
+		 [KVAL(K_COMPOSE)]      = NULL,
+		 [KVAL(K_SAK)]          = NULL /* https://en.wikipedia.org/wiki/Secure_attention_key, ignore it */,
+		 [KVAL(K_DECRCONSOLE)]  = NULL /* Switch to previous VT. Fat chance! */,
+		 [KVAL(K_INCRCONSOLE)]  = NULL /* Switch to next VT. Fat chance! */,
 		 [KVAL(K_SPAWNCONSOLE)] = NULL /* Spawn and switch to a new VT. Fat chance! */,
-		 [KVAL(K_BARENUMLOCK)] = NULL /* TODO: What is this? */,
-		 [KVAL(K_ALLOCATED)] = NULL /* do nothing (see <linux/keyboard.h> for more information) */,
-		 [KVAL(K_NOSUCHMAP)] = NULL /* do nothing (see <linux/keyboard.h> for more information) */},
+		 [KVAL(K_BARENUMLOCK)]  = NULL /* TODO: What is this? */,
+		 [KVAL(K_ALLOCATED)]    = NULL /* do nothing (see <linux/keyboard.h> for more information) */,
+		 [KVAL(K_NOSUCHMAP)]    = NULL /* do nothing (see <linux/keyboard.h> for more information) */,
+		 NULL /* we always put NULL at the end so all invalid onces will not do things. */},
   [KT_PAD]    = {[KVAL(K_P0)] = "0",
 		 [KVAL(K_P1)] = "1",
 		 [KVAL(K_P2)] = "2",
@@ -75,36 +76,40 @@ const char* KVAL_MAP[15][256] = {
 		 [KVAL(K_P7)] = "7",
 		 [KVAL(K_P8)] = "8",
 		 [KVAL(K_P9)] = "9",
-		 [KVAL(K_PPLUS)] = "+",
-		 [KVAL(K_PMINUS)] = "-",
-		 [KVAL(K_PSTAR)] = "*",
-		 [KVAL(K_PSLASH)] = "/",
-		 [KVAL(K_PENTER)] = "\n",
-		 [KVAL(K_PCOMMA)] = ",",
-		 [KVAL(K_PDOT)] = ".",
+		 [KVAL(K_PPLUS)]      = "+",
+		 [KVAL(K_PMINUS)]     = "-",
+		 [KVAL(K_PSTAR)]      = "*",
+		 [KVAL(K_PSLASH)]     = "/",
+		 [KVAL(K_PENTER)]     = "\n",
+		 [KVAL(K_PCOMMA)]     = ",",
+		 [KVAL(K_PDOT)]       = ".",
 		 [KVAL(K_PPLUSMINUS)] = "±",
-		 [KVAL(K_PPARENL)] = "(",
-		 [KVAL(K_PPARENR)] = ")"},
+		 [KVAL(K_PPARENL)]    = "(",
+		 [KVAL(K_PPARENR)]    = ")",
+		 NULL},
   [KT_DEAD]   = {[KVAL(K_DGRAVE)] = NULL,
 		 [KVAL(K_DACUTE)] = NULL,
 		 [KVAL(K_DCIRCM)] = NULL,
 		 [KVAL(K_DTILDE)] = NULL,
 		 [KVAL(K_DDIERE)] = NULL,
-		 [KVAL(K_DCEDIL)] = NULL},
+		 [KVAL(K_DCEDIL)] = NULL,
+		 NULL},
   [KT_CONS]   = {NULL},
-  [KT_CUR]    = {[KVAL(K_DOWN)] = "\033[B",
-		 [KVAL(K_LEFT)] = "\033[D",
+  [KT_CUR]    = {[KVAL(K_DOWN)]  = "\033[B",
+		 [KVAL(K_LEFT)]  = "\033[D",
 		 [KVAL(K_RIGHT)] = "\033[C",
-		 [KVAL(K_UP)] = "\033[A"},
-  [KT_SHIFT]  = {[KVAL(K_SHIFT)] = NULL,
-		 [KVAL(K_CTRL)] = NULL,
-		 [KVAL(K_ALT)] = NULL,
-		 [KVAL(K_ALTGR)] = NULL,
-		 [KVAL(K_SHIFTL)] = NULL,
-		 [KVAL(K_SHIFTR)] = NULL,
-		 [KVAL(K_CTRLL)] = NULL,
-		 [KVAL(K_CTRLR)] = NULL,
-		 [KVAL(K_CAPSSHIFT)] = NULL},
+		 [KVAL(K_UP)]    = "\033[A",
+		 NULL},
+  [KT_SHIFT]  = {[KVAL(K_SHIFT)]     = NULL,
+		 [KVAL(K_CTRL)]      = NULL,
+		 [KVAL(K_ALT)]       = NULL,
+		 [KVAL(K_ALTGR)]     = NULL,
+		 [KVAL(K_SHIFTL)]    = NULL,
+		 [KVAL(K_SHIFTR)]    = NULL,
+		 [KVAL(K_CTRLL)]     = NULL,
+		 [KVAL(K_CTRLR)]     = NULL,
+		 [KVAL(K_CAPSSHIFT)] = NULL,
+		 NULL},
   [KT_META]   = {NULL},
   [KT_ASCII]  = {[KVAL(K_ASC0)] = NULL,
 		 [KVAL(K_ASC1)] = NULL,
@@ -131,37 +136,41 @@ const char* KVAL_MAP[15][256] = {
 		 [KVAL(K_HEXc)] = NULL,
 		 [KVAL(K_HEXd)] = NULL,
 		 [KVAL(K_HEXe)] = NULL,
-		 [KVAL(K_HEXf)] = NULL},
-  [KT_LOCK]   = {[KVAL(K_SHIFTLOCK)] = NULL,
-		 [KVAL(K_CTRLLOCK)] = NULL,
-		 [KVAL(K_ALTLOCK)] = NULL,
-		 [KVAL(K_ALTGRLOCK)] = NULL,
-		 [KVAL(K_SHIFTLLOCK)] = NULL,
-		 [KVAL(K_SHIFTRLOCK)] = NULL,
-		 [KVAL(K_CTRLLLOCK)] = NULL,
-		 [KVAL(K_CTRLRLOCK)] = NULL,
-		 [KVAL(K_CAPSSHIFTLOCK)] = NULL},
-  [KT_SLOCK]  = {[KVAL(K_SHIFT_SLOCK)] = NULL,
-		 [KVAL(K_CTRL_SLOCK)] = NULL,
-		 [KVAL(K_ALT_SLOCK)] = NULL,
-		 [KVAL(K_ALTGR_SLOCK)] = NULL,
-		 [KVAL(K_SHIFTL_SLOCK)] = NULL,
-		 [KVAL(K_SHIFTR_SLOCK)] = NULL,
-		 [KVAL(K_CTRLL_SLOCK)] = NULL,
-		 [KVAL(K_CTRLR_SLOCK)] = NULL,
-		 [KVAL(K_CAPSSHIFT_SLOCK)] = NULL},
+		 [KVAL(K_HEXf)] = NULL,
+		 NULL},
+  [KT_LOCK]   = {[KVAL(K_SHIFTLOCK)]     = NULL,
+		 [KVAL(K_CTRLLOCK)]      = NULL,
+		 [KVAL(K_ALTLOCK)]       = NULL,
+		 [KVAL(K_ALTGRLOCK)]     = NULL,
+		 [KVAL(K_SHIFTLLOCK)]    = NULL,
+		 [KVAL(K_SHIFTRLOCK)]    = NULL,
+		 [KVAL(K_CTRLLLOCK)]     = NULL,
+		 [KVAL(K_CTRLRLOCK)]     = NULL,
+		 [KVAL(K_CAPSSHIFTLOCK)] = NULL,
+		 NULL},
+  [KT_SLOCK]  = {[KVAL(K_SHIFT_SLOCK)]     = NULL,
+		 [KVAL(K_CTRL_SLOCK)]      = NULL,
+		 [KVAL(K_ALT_SLOCK)]       = NULL,
+		 [KVAL(K_ALTGR_SLOCK)]     = NULL,
+		 [KVAL(K_SHIFTL_SLOCK)]    = NULL,
+		 [KVAL(K_SHIFTR_SLOCK)]    = NULL,
+		 [KVAL(K_CTRLL_SLOCK)]     = NULL,
+		 [KVAL(K_CTRLR_SLOCK)]     = NULL,
+		 [KVAL(K_CAPSSHIFT_SLOCK)] = NULL,
+		 NULL},
   [KT_DEAD2]  = {NULL},
   [KT_BRL]    = {[KVAL(K_BRL_BLANK)] = NULL,
-		 [KVAL(K_BRL_DOT1)] = NULL,
-		 [KVAL(K_BRL_DOT2)] = NULL,
-		 [KVAL(K_BRL_DOT3)] = NULL,
-		 [KVAL(K_BRL_DOT4)] = NULL,
-		 [KVAL(K_BRL_DOT5)] = NULL,
-		 [KVAL(K_BRL_DOT6)] = NULL,
-		 [KVAL(K_BRL_DOT7)] = NULL,
-		 [KVAL(K_BRL_DOT8)] = NULL,
-		 [KVAL(K_BRL_DOT9)] = NULL,
-		 [KVAL(K_BRL_DOT10)] = NULL}
+		 [KVAL(K_BRL_DOT1)]  = NULL,
+		 [KVAL(K_BRL_DOT2)]  = NULL,
+		 [KVAL(K_BRL_DOT3)]  = NULL,
+		 [KVAL(K_BRL_DOT4)]  = NULL,
+		 [KVAL(K_BRL_DOT5)]  = NULL,
+		 [KVAL(K_BRL_DOT6)]  = NULL,
+		 [KVAL(K_BRL_DOT7)]  = NULL,
+		 [KVAL(K_BRL_DOT8)]  = NULL,
+		 [KVAL(K_BRL_DOT9)]  = NULL,
+		 [KVAL(K_BRL_DOT10)] = NULL,
+		 NULL}
 };
 
 
@@ -211,7 +220,9 @@ int main(int argc, char** argv)
       if (!pid)
 	verifier(fd_child);
       
-      alarm(10); /* while testing, we are aborting after 60 seconds, you can also quit with 'q' */
+      fd = STDOUT_FILENO; /* This is just testing. */
+      
+      alarm(60); /* while testing, we are aborting after 60 seconds, you can also quit with 'q' */
       printf("\n    Enter passphrase: ");
       fflush(stdout);
       
@@ -234,9 +245,6 @@ int main(int argc, char** argv)
 	    continue;
 	  c = key_maps[modifiers][c] & 0x0FFF;
 	  
-	  fd = STDOUT_FILENO; /* This is just testing. */
-	  
-	  /* /usr/include/linux/keyboard.h is interresting here... */
 	  switch (KTYP(c))
 	    {
 	    case KT_LETTER: /* Symbols that are affected by the Royal Canterlot Voice key */
@@ -252,13 +260,11 @@ int main(int argc, char** argv)
 	      break;
 	      
 	    case KT_FN:     /* Customisable keys, usally for escape sequnces. Includes F-keys and some misc. keys */
-	      fdprint(fd, func_table[KVAL(c)]);
+	      if (func_table[KVAL(c)] != NULL)
+		fdprint(fd, func_table[KVAL(c)]);
 	      break;
 	      
-	    case KT_SHIFT:  /* A modifier is used */
-	      break;
-	      
-	    case KT_DEAD:   /* Dead key, can we utilise Linux for this? */
+	    case KT_DEAD:   /* TODO: Dead key, can we utilise Linux for this? */
 	      break;
 	      
 	    case KT_SPEC:   /* Special keys*/
@@ -267,15 +273,26 @@ int main(int argc, char** argv)
 	    case KT_ASCII:  /* This is what happens when somepony holds down Alternative whil using the keypad */
 	      if (KVAL_MAP[KTYP(c)][KVAL(c)] != NULL)
 		fdprint(fd, KVAL_MAP[KTYP(c)][KVAL(c)]);
+	      else if (KTYP(c) == KT_SPEC)
+		switch (c)
+		  {
+		  case K_COMPOSE: /* TODO: Compose key, can we utilise Linux for this? */
+		    break;
+		    
+		  case K_NUM:     /* TODO: Num Lock */
+		    break;
+		    
+		  default: /* Other keys do nothing */
+		    break;
+		  }
 	      break;
 	      
+	    case KT_SHIFT:  /* A modifier is used, we took care about this before the switch, so this should not happen */
 	    case KT_CONS:   /* Somepony is trying to switch VT. Fat chance! */
-	    case KT_LOCK:   /* TODO: What is this? */
-	    case KT_SLOCK:  /* TODO: What is this? */
+	    case KT_LOCK:   /* TODO: Is this sticky keys that toggle? */
+	    case KT_SLOCK:  /* TODO: Is this sticky keys that resemble dead keys? */
 	    case KT_DEAD2:  /* TODO: What is this? */
 	    case KT_BRL:    /* Braille, how does this work? */
-	      break;
-	      
 	    default:        /* What?! This should not happen! */
 	      break;
 	    }
